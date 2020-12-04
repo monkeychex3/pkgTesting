61b129a7876fc818c6640e96dab71fac8cbf9720
acoger <acoger@bgsu.edu>
2020-12-04 01:27:15 -0500

    add dateRange functionality

--- R/app_server.R
+++ R/app_server.R
@@ -6,15 +6,19 @@ app_server <- function(input, output, session) {
   output$graphPlaceholder <- renderText("safety text for graph tab")
 
   cdata <- eventReactive(input$load,{
-    temp <- COVID19::covid19(country = "us", level = 2, start = "2020-12-1", verbose = FALSE)
-    temp <- temp[ , c(2:10,29)]
+    temp <- COVID19::covid19(
+      country = "us", level = 2, start = sort(input$dateRange)[1],
+      end = sort(input$dateRange)[2], verbose = FALSE
+    )
+    temp <- temp[ , c(2:10,29)] #discards id and mostly NA columns
     temp
   })
 
-  output$table <- renderPrint(summary(
-    cdata()[cdata()$administrative_area_level_2 %in% input$state, ]
+  output$summary <- renderPrint({
+    summary(
+      cdata()[cdata()$administrative_area_level_2 %in% input$state, ]
     )
-  )
+  })
 
   output$state_ui <- renderUI({
     checkboxGroupInput(inputId = "state", label = "Choose States:",
--- R/app_ui.R
+++ R/app_ui.R
@@ -1,42 +1,54 @@
 #' @import shiny
 
 app_ui <- function() {
-  fluidPage(
+  # fluidPage(
+  #   theme = shinythemes::shinytheme("darkly")
+  # )
+  navbarPage(
+    title = "This is Navbar placeholder text",
     theme = shinythemes::shinytheme("darkly"),
-    navbarPage(
-      title = "This is Navbar placeholder text",
-      tabPanel(
-        title = "select",
+    tabPanel(
+      title = "select",
+      fluidRow(
         column(
           width = 4,
           HTML("<br><br>"),
           textOutput("selectPlaceholder"),
           HTML("<br><br>"),
-          dateRangeInput(inputId = "dateRange", label = "Date Range to Load"),
+          dateRangeInput(
+            inputId = "dateRange", label = "Date Range to Load:",
+            start = "2020-02-01",end = Sys.Date(),
+            min = "2020-02-01", max = Sys.Date()
+          ),
           actionButton(inputId = "load", label = "Load COVID Data")
         ),
         column(
-          width = 3.5,
+          width = 4,
           uiOutput("state_ui")
-        )
-      ),
-      tabPanel(
-        title = "edit",
+        ),
         column(
           width = 4,
-          HTML("<br><br>"),
-          textOutput("editPlaceholder"),
-          HTML("<br><br>")
+          verbatimTextOutput("summary")
         )
+      )
+    ),
+    tabPanel(
+      title = "edit",
+      column(
+        width = 4,
+        HTML("<br><br>"),
+        textOutput("editPlaceholder"),
+        HTML("<br><br>")
       ),
-      tabPanel(
-        title = "graph",
-        column(
-          width = 4,
-          HTML("<br><br>"),
-          textOutput("graphPlaceholder"),
-          HTML("<br><br>")
-        )
+
+    ),
+    tabPanel(
+      title = "graph",
+      column(
+        width = 4,
+        HTML("<br><br>"),
+        textOutput("graphPlaceholder"),
+        HTML("<br><br>")
       )
     )
   )
