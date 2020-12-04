a0216198261859b72b0b10e9e87a69108ad55379
acoger <acoger@bgsu.edu>
2020-12-04 00:03:04 -0500

    update layout to navbar page

--- R/app_server.R
+++ R/app_server.R
@@ -1,7 +1,9 @@
 #' @import shiny
 
 app_server <- function(input, output, session) {
-  output$text <- renderText("safety text")
+  output$selectPlaceholder <- renderText("safety text for select tab")
+  output$editPlaceholder <- renderText("safety text for edit tab")
+  output$graphPlaceholder <- renderText("safety text for graph tab")
 
   cdata <- eventReactive(input$load,{
     temp <- COVID19::covid19(country = "us", level = 2, start = "2020-12-1", verbose = FALSE)
@@ -16,8 +18,7 @@ app_server <- function(input, output, session) {
 
   output$state_ui <- renderUI({
     checkboxGroupInput(inputId = "state", label = "Choose States:",
-      choices = sort(unique(cdata()$administrative_area_level_2)),
-      choiceNames = sort(unique(cdata()$administrative_area_level_2))
+      choices = sort(unique(cdata()$administrative_area_level_2))
     )
   })
 }
--- R/app_ui.R
+++ R/app_ui.R
@@ -3,17 +3,39 @@
 app_ui <- function() {
   fluidPage(
     navbarPage(
-      title = "hello world"
-    ),
-
-    sidebarLayout(
-      sidebarPanel(
-        uiOutput("state_ui")
+      title = "This is Navbar placeholder text",
+      tabPanel(
+        title = "select",
+        column(
+          width = 4,
+          HTML("<br><br>"),
+          textOutput("selectPlaceholder"),
+          HTML("<br><br>"),
+          dateRangeInput(inputId = "dateRange", label = "Date Range to Load"),
+          actionButton(inputId = "load", label = "Load COVID Data")
+        ),
+        column(
+          width = 3.5,
+          uiOutput("state_ui")
+        )
       ),
-      mainPanel(
-        actionButton(inputId = "load", label = "Load COVID Data"),
-        textOutput("text"),
-        verbatimTextOutput("table")
+      tabPanel(
+        title = "edit",
+        column(
+          width = 4,
+          HTML("<br><br>"),
+          textOutput("editPlaceholder"),
+          HTML("<br><br>")
+        )
+      ),
+      tabPanel(
+        title = "graph",
+        column(
+          width = 4,
+          HTML("<br><br>"),
+          textOutput("graphPlaceholder"),
+          HTML("<br><br>")
+        )
       )
     )
   )
